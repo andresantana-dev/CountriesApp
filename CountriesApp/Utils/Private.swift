3cea8cfb22e9b0c136e06d7edf75610372886a61
André Santana <andre.santana@polygon.pt>
2021-06-15 16:13:23 +0100

    add Private class

---
+++ CountriesApp/Utils/Private.swift
@@ -0,0 +1,12 @@
+//
+//  Private.swift
+//  CountriesApp
+//
+//  Created by André Santana on 13/06/2021.
+//
+
+import Foundation
+
+struct Private {
+    static let apiKey = "1acadfdef6mshfc18a1c00eaff00p1b782fjsna527cdcd4e4d"
+}
